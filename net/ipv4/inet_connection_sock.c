9ee5fe207969216c2db387cc55463e6871c1ec40
fadlyas07 <fadlyardhians@gmail.com>
2019-12-23 10:36:44 +0700

    Revert "net: Fail explicit bind to local reserved ports"
    This reverts commit 4cd79043f863f0f555f1e5f80fe59cbe0efc157a.

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -170,11 +170,6 @@ have_snum:
 				hashinfo->bhash_size)];
 		spin_lock(&head->lock);
 
-		if (inet_is_local_reserved_port(net, snum) &&
-		    !sysctl_reserved_port_bind) {
-			goto fail_unlock;
-		}
-
 		inet_bind_bucket_for_each(tb, &head->chain)
 			if (net_eq(ib_net(tb), net) && tb->port == snum)
 				goto tb_found;
